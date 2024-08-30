d9f466b316ea60e845e0ffde289272bf8c0e4461
krnmalde2 <krnmalde2@gmail.com>
2024-08-30 12:10:00 +0000

    Update variable

---
+++ basics1/variable.js
@@ -0,0 +1,13 @@
+var studentId = 22115
+let studentName = "hitesh"
+const rollNo = 22
+
+console.log("Name of the student is "+studentName)
+//console.table([studentId,studentName,rollNo])
+
+console.log(typeof(studentId))
+let studentId2 = Boolean(studentId)
+console.log(typeof(studentId2))
+console.log(studentId2)
+
+
