95da8f0c100bfd65e2ab46c0791a7318e892f292
krnmalde2 <krnmalde2@gmail.com>
2024-08-30 12:23:41 +0000

    added datatypes

---
+++ basics1/datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//alert("3+3") we are using node js engine not browser therefpre alert give error
+
+/*
+Int -> 2^53
+bigInt
+strings -> "",''
+boolean -> true,false
+undefined
+null -> Standlone Value means empty
+symbol -> to uniquely identify components
+*/
+
+//object
+let studentName = "Hitesh"
+let rollNo = 25
+let isPresent = true
+let Result
+let busNo = null
+
+
+//console.table([studentName,rollNo,isPresent,Result,busNo]);
+
+console.log(typeof busNo) //null is object
+console.log(typeof Result) //undefined is undefined
+console.log(typeof isPresent) //boolean is boolean
